86cefcc07af9186f225f3ad889b5a65f41276497
Sergey Ilyashenko <sergey.ilyashenko@zensoft.io>
2018-07-26 17:15:11 +0300

    typescript added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  public render() {
     return (
       <div className="App">
         <header className="App-header">
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
       </div>
     );
