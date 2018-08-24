981b1d6e9ded86c6f01d3c03b60e0d2a0ed5ec52
Sergey Ilyashenko <sergey.ilyashenko@zensoft.io>
2018-08-24 12:06:45 +0300

    OP-243 Delegate Forms

--- src/components/BecomeDelegateForm.tsx
+++ src/components/BecomeDelegateForm.tsx
@@ -3,11 +3,35 @@ import { DelegateTabs } from './DelegateTabs';
 
 export class BecomeDelegateForm extends React.Component<{}> {
   public render() {
+    const feeError = 'Fee Error';
+
     return (
       <div className="left-section">
         <form className="delegate-form">
           <DelegateTabs />
-          Become Delegate Functionality
+          <div className="BaD-section">
+            <h2>
+              Become <br />a Delegate
+            </h2>
+            <div className="input">
+              <p>FROM</p>
+              <input
+                className="disable"
+                type="text"
+                placeholder="Wallet Address"
+                value="0XFF7508C54D3EF2141D05F7EB1A0CC719"
+              />
+            </div>
+            <div className={`input ${feeError && 'invalid'}`}>
+              <p className="required">Fee Amount</p>
+              <span className="error">{feeError}</span>
+              <input className="" type="text" placeholder="Fee Amount" required={true} />
+            </div>
+          </div>
+          <button className="button mini">
+            <div />
+            <span>confirm</span>
+          </button>
         </form>
       </div>
     );
--- src/components/VoteForm.tsx
+++ src/components/VoteForm.tsx
@@ -3,11 +3,30 @@ import { DelegateTabs } from './DelegateTabs';
 
 export class VoteForm extends React.Component<{}> {
   public render() {
+    const delegateError = 'Delegate Error';
+    const feeError = 'Fee Error';
+
     return (
       <div className="left-section">
         <form className="delegate-form">
           <DelegateTabs />
-          Vote Form
+          <div className="vote-section">
+            <h2>Vote</h2>
+            <div className={`input ${delegateError && 'invalid'}`}>
+              <p className="required">Delegate's address</p>
+              <span className="error">{delegateError}</span>
+              <input className="" type="text" placeholder="Wallet" required={true} />
+            </div>
+            <div className={`input ${feeError && 'invalid'}`}>
+              <p className="required">Fee Amount</p>
+              <span className="error">{feeError}</span>
+              <input className="" type="text" placeholder="Fee Amount" required={true} />
+            </div>
+            <button className="button mini">
+              <div />
+              <span>confirm</span>
+            </button>
+          </div>
         </form>
       </div>
     );
