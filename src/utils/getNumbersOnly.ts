00fc66981a897877f1005e7491de28a454eb4fb6
Pavel Anosov <pavel.anosov@zensoft.io>
2018-09-14 11:09:54 +0300

    Add validation for Amount and Fee amount field: Do not allow input zero values Fractional numbers

--- src/utils/getNumbersOnly.ts
+++ src/utils/getNumbersOnly.ts
@@ -1,7 +1,7 @@
 export const getNumbersOnly = (value: string) => {
-  const result = value.replace(/[^0-9.]+/g, '');
-  if (result.split('').filter(it => it === '.').length > 1) {
-    return result.replace(/[.]/, '');
+  const result = value.replace(/[^0-9]+/g, '');
+  if (+result === 0) {
+    return '';
   }
   return result;
 };
