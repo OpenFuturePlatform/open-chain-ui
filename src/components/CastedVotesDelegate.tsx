9afee4127e20555be588efc2ca8ea7c3b5bc910c
Pavel Anosov <pavel.anosov@zensoft.io>
2019-01-24 15:23:13 +0300

    ...

--- src/components/CastedVotesDelegate.tsx
+++ src/components/CastedVotesDelegate.tsx
@@ -10,7 +10,9 @@ interface IProps {
 }
 
 export const CastedVotesDelegate = ({ delegate, recallVoteDelegate, isRecallButtonDisabled }: IProps) => {
-  if(!delegate) return null;
+  if (!delegate) {
+      return null;
+  }
   const onCopyHandler = (value: string) => copy2Clipboard(value);
 
   return (
